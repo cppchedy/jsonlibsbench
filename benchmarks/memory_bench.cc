4c402134af5c9fb8742a93932a5d1758990a61b4
Chedy Najjar <chedy@vectorized.io>
2020-03-03 17:27:33 +0100

    change target memory_bench to memory_bench_heaptrack.

--- benchmarks/memory_bench.cc
+++ benchmarks/memory_bench.cc
@@ -5,30 +5,11 @@
 #include <rapidjson/document.h>
 #include <sajson.h>
 
+#include "utility.h"
 
 using namespace simdjson;
 
 using FN_TYPE = bool (*)(const char*);
-
-std::pair<char *, size_t> open_file(const char *file_name){
-    FILE *file = fopen(file_name, "rb");
-    if (!file){
-        fprintf(stderr, "Failed to open file\n");
-        return {};
-    }
-    fseek(file, 0, SEEK_END);
-    size_t length = ftell(file);
-    fseek(file, 0, SEEK_SET);
-
-    char *buffer = new char[length];
-
-    if (length != fread(buffer, 1, length, file)){
-        fprintf(stderr, "Failed to read entire file\n");
-        return {};
-    }
-    fclose(file);
-    return {buffer, length};
-}
 
 
 //SIMDJson
